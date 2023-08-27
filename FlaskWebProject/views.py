c5ca79032b6a12296d5834084150afc84662bdaf
chauitvn <chaunn3502@gmail.com>
2023-08-27 16:00:01 +0800

    update warining to the log

--- FlaskWebProject/views.py
+++ FlaskWebProject/views.py
@@ -66,11 +66,12 @@ def login():
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.username.data).first()
         if user is None or not user.check_password(form.password.data):
-            app.logger.info('Invalid login attempt')
+            app.logger.warning('Invalid login attempt')
             flash('Invalid username or password')
             return redirect(url_for('login'))
+        
         login_user(user, remember=form.remember_me.data)
-        app.logger.info('Logged in successfully')
+        app.logger.warning('Logged in successfully')
 
         next_page = request.args.get('next')
         if not next_page or url_parse(next_page).netloc != '':
@@ -104,6 +105,9 @@ def authorized():
 @app.route('/logout')
 def logout():
     logout_user()
+
+    app.logger.warning('Successful User logout!')
+    
     if session.get("user"): # Used MS Login
         # Wipe out user and its token cache from session
         session.clear()
